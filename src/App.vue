13fe5a0af8960e892362f0fbd56182cc3612d76b
Kwasu <lukaszkwas96@gmail.com>
2025-01-31 15:25:59 +0100

    .

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,26 @@
     onsubmit="return false"
   >
     <header class="w-full flex justify-center items-center gap-6">
-      <hr class="grow border-[1px] border-teal-lighter" />
+      <hr class="grow h-[2px] border-none bg-teal-lighter" />
       <h1 class="font-extrabold text-4xl text-center text-teal uppercase">
         Formularz zgłoszeniowy
       </h1>
-      <hr class="grow border-[1px] border-teal-lighter" />
+      <hr class="grow h-[2px] border-none bg-teal-lighter" />
     </header>
-    <fieldset class="flex flex-col items-start gap-6 w-full">
+    <fieldset class="flex flex-col items-start gap-6 w-full" id="personal-data">
       <h2 class="font-bold block text-[26px] text-gray-darker">Dane osobowe</h2>
       <div
         class="flex flex-col md:flex-row items-start md:items-center justify-start gap-5 md:gap-[10px] w-full text-gray-darker"
       >
         <div class="w-[125px] shrink-0 h-10 relative text-gray-mid">
+          <Transition name="fade">
+            <DropDown
+              class="absolute top-11 left-0 z-10"
+              v-if="isDropdownVisible"
+              @onSelect="selectTitle"
+              @close="isDropdownVisible = false"
+            ></DropDown>
+          </Transition>
           <label
             for="title"
             class="text-xxs absolute -top-2 left-2 bg-white z-10 px-[5px]"
@@ -24,11 +32,19 @@
           <button
             id="title"
             class="w-full h-full border-[1px] border-gray-mid rounded-lg relative cursor-pointer"
+            @click="isDropdownVisible = !isDropdownVisible"
           >
             <div
               class="absolute h-full top-0 left-2 px-[5px] flex items-center justify-center italic"
+              v-if="!title"
             >
               Wybierz
+            </div>
+            <div
+              class="absolute h-full top-0 left-2 px-[5px] flex items-center justify-center text-black"
+              v-else
+            >
+              {{ title }}
             </div>
             <div
               class="absolute h-full flex items-center justify-center right-3 top-0"
@@ -59,7 +75,6 @@
             class="w-full h-full border-[1px] border-gray-mid rounded-lg relative px-[13px] py-2.5 placeholder:italic placeholder:text-gray-mid outline-none text-black"
             type="text"
             placeholder="Imię"
-            pattern="[A-Za-z]"
           />
         </div>
         <div class="w-full h-10 relative text-gray-mid">
@@ -76,18 +91,104 @@
             class="w-full h-full border-[1px] border-gray-mid rounded-lg relative px-[13px] py-2.5 placeholder:italic placeholder:text-gray-mid outline-none text-black"
             type="text"
             placeholder="Nazwisko"
-            pattern="[A-Za-z]"
           />
         </div>
       </div>
     </fieldset>
-    <fieldset class="flex flex-col items-start gap-6 w-full">
-      <h2 class="font-bold block text-[26px] text-gray-darker">Zgody</h2>
-      <div
-        class="flex items-center justify-start gap-[10px] w-full text-gray-darker"
-      ></div>
+    <fieldset
+      class="flex flex-col items-start gap-6 w-full"
+      id="travel-destination"
+    >
+      <h2 class="font-bold block text-[26px] text-gray-darker">Cel podróży</h2>
+
+      <h4 class="text-black">
+        Proszę wybrać miejsce docelowe. <span class="text-red-500">*</span>
+      </h4>
+
+      <div class="w-full justify-between"></div>
     </fieldset>
-    <hr class="w-full border-[1px] border-teal-lighter" />
+    <fieldset class="flex flex-col items-start gap-6 w-full" id="consents">
+      <h2 class="font-bold block text-[26px] text-gray-darker">Zgody</h2>
+
+      <label
+        class="inline-flex w-full gap-6 items-center justify-start cursor-pointer"
+      >
+        <input
+          type="checkbox"
+          class="sr-only peer"
+          v-model="consentData"
+          true-value="yes"
+          false-value=""
+        />
+        <div
+          class="shrink-0 relative w-7 h-[18px] bg-white border-[1px] border-gray-mid rounded-full peer peer-checked:after:translate-x-[calc(100%-4px)] after:content-[''] after:absolute after:top-[calc(50%-7px)] after:start-[1px] after:bg-gray-mid after:rounded-full after:h-[14px] after:w-[14px] after:transition-all after:duration-300 transition-all duration-300 peer-checked:after:bg-teal peer-checked:border-teal"
+        ></div>
+        <div class="flex flex-col gap-[5px] w-full items-start">
+          <div class="w-full flex justify-center items-center gap-[10px]">
+            <p
+              class="text-sm font-bold"
+              :class="consentData ? 'text-teal-darker' : 'text-gray-darker'"
+            >
+              Zgoda na przetwarzanie danych <span class="text-red-500">*</span>
+            </p>
+            <hr
+              class="grow h-[1px] border-none"
+              :class="consentData ? 'bg-teal-lighter' : 'bg-gray-lighter'"
+            />
+          </div>
+          <div class="text-xs text-black text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
+      </label>
+
+      <label
+        class="inline-flex w-full gap-6 items-center justify-start cursor-pointer"
+      >
+        <input
+          type="checkbox"
+          class="sr-only peer"
+          v-model="consentMarketing"
+          true-value="yes"
+          false-value=""
+        />
+        <div
+          class="shrink-0 relative w-7 h-[18px] bg-white border-[1px] border-gray-mid rounded-full peer peer-checked:after:translate-x-[calc(100%-4px)] after:content-[''] after:absolute after:top-[calc(50%-7px)] after:start-[1px] after:bg-gray-mid after:rounded-full after:h-[14px] after:w-[14px] after:transition-all after:duration-300 transition-all duration-300 peer-checked:after:bg-teal peer-checked:border-teal"
+        ></div>
+        <div class="flex flex-col gap-[5px] w-full items-start">
+          <div class="w-full flex justify-center items-center gap-[10px]">
+            <p
+              class="text-sm font-bold"
+              :class="
+                consentMarketing ? 'text-teal-darker' : 'text-gray-darker'
+              "
+            >
+              Zgody marketingowe
+            </p>
+            <hr
+              class="grow h-[1px] border-none"
+              :class="consentMarketing ? 'bg-teal-lighter' : 'bg-gray-lighter'"
+            />
+          </div>
+          <div class="text-xs text-black text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
+      </label>
+    </fieldset>
+    <hr class="w-full h-[2px] border-none bg-teal-lighter" />
     <button
       class="uppercase rounded-md py-[10px] px-[13px] text-white text-sm font-bold transition-colors duration-300"
       :class="
@@ -105,16 +206,32 @@
 
 <script setup lang="ts">
 import { ref, computed } from "vue";
-import { Title } from "./main";
+import type { Title } from "./main.ts";
+import DropDown from "./components/DropDown.vue";
 
-const title = ref<Title | null>("Pan");
+const title = ref<Title | null>(null);
+const selectTitle = (selectedTitle: Title) => {
+  title.value = selectedTitle;
+  isDropdownVisible.value = false;
+};
+
+const isDropdownVisible = ref(false);
+
 const firstName = ref("");
 const lastName = ref("");
 
 const formatName = (name: string) => name.replace(/[^\p{L}]/gu, "");
 
+const consentData = ref<"yes" | "">("");
+const consentMarketing = ref<"yes" | "">("");
+
 const isFormValid = computed(() => {
-  if (firstName.value.length < 3 || lastName.value.length < 3 || !title.value) {
+  if (
+    firstName.value.length < 3 ||
+    lastName.value.length < 3 ||
+    !title.value ||
+    !consentData.value
+  ) {
     return false;
   } else {
     return true;
---
+++ src/components/DropDown.vue
@@ -0,0 +1,66 @@
+<template>
+  <ul
+    class="w-[125px] bg-white text-black border-[1px] border-gray-mid rounded-lg"
+    ref="dropDownRef"
+  >
+    <li
+      class="cursor-pointer hover:font-medium w-full pt-2 h-full px-[10px]"
+      @click="emits('onSelect', 'bez tytułu')"
+    >
+      bez tytułu
+    </li>
+    <li
+      class="cursor-pointer hover:font-medium w-full h-full px-[10px]"
+      @click="emits('onSelect', 'Pan')"
+    >
+      Pan
+    </li>
+    <li
+      class="cursor-pointer hover:font-medium w-full h-full px-[10px]"
+      @click="emits('onSelect', 'Pani')"
+    >
+      Pani
+    </li>
+    <li
+      class="cursor-pointer hover:font-medium w-full h-full px-[10px]"
+      @click="emits('onSelect', 'Dr')"
+    >
+      Dr
+    </li>
+    <li
+      class="cursor-pointer hover:font-medium w-full pb-2 h-full px-[10px]"
+      @click="emits('onSelect', 'Prof')"
+    >
+      Prof.
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+const emits = defineEmits(["onSelect", "close"]);
+
+const dropDownRef = ref<HTMLUListElement | null>(null);
+
+const handleClickOutside = (e: Event) => {
+  if (
+    dropDownRef.value &&
+    e.target instanceof Node &&
+    !dropDownRef.value.contains(e.target)
+  ) {
+    emits("close");
+    document.removeEventListener("click", handleClickOutside);
+    document.removeEventListener("touch", handleClickOutside);
+  }
+};
+
+onMounted(() => {
+  if (dropDownRef.value) {
+    setTimeout(() => {
+      document.addEventListener("click", handleClickOutside);
+    }, 0);
+  } else {
+    document.removeEventListener("click", handleClickOutside);
+  }
+});
+</script>
